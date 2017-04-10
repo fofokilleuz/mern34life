6bca9fa153d9c8c4734220a77cbade805e6a694b
Faustine Geoffray <faustinegeoffray@iMac-de-Yahel.local>
2017-04-10 10:51:49 +0200

    fix issue

--- src/comment/CommentList.js
+++ src/comment/CommentList.js
@@ -4,15 +4,15 @@ import Comment from './Comment';
 
 class CommentList extends Component {
   render() {
-    let commentNodes = this.props.data.map(comment => {
+    let commentNodes = this.props.data.map((comment, index) => {
       return (
         <Comment
+          key={ index }
           author={ comment.author }
           text={ comment.text }
           uniqueID={ comment['_id'] }
           onCommentDelete={ this.props.onCommentDelete }
-          onCommentUpdate={ this.props.onCommentUpdate }
-          key={ comment['_id'] }>
+          onCommentUpdate={ this.props.onCommentUpdate }>
           { comment.text }
         </Comment>
       )
